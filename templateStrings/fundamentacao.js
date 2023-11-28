cd297f6cfd7434bf12a62bdafeb06656006205eb
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2023-11-27 23:04:53 -0300

    Atualização Final do Projeto da Urna Eletronica

--- templateStrings/fundamentacao.js
+++
@@ -1,8 +0,0 @@
-//As "templates Strings" em JavaScript são uma forma conveniente de criar strings que permitem interpolar de variáveis ou expressões direntamente em uma strint. Elas são criadas usando delimitadores de crase (``) em vez das aspas simples ou duplas tradicionais (`!` ou "")
-
-const testes = document.querySelector("#Testes")
-
-const string = "Template String"
-
-testes.innerHTML = string
-
