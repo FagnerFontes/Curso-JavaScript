96606403f97bd615c52988444c293c9c94e012b1
FagnerFontes <fagnerfontesbatista@hotmail.com>
2024-12-22 16:33:18 -0300

    Atualização 22/11/2024

--- promisse/fundamentação/fundamentacao.js
+++ promisse/fundamentação/fundamentacao.js
@@ -1,27 +1,60 @@
 const myPromise = new Promise((resolve, reject) =>{
-    //Silmulando uma operação assíncrona que leva 1 segundo para ser  concluída. 
+   
+    //Cria uma nova Promise. A função passada para o contrutor recebe dois parâmetros. 
+    //'resolve' é chamado para marcar a Promise como resolvida 
+    // 'reject' é chamado para marcar a Promise como rejeitada
+     //Silmulando uma operação assíncrona que leva 1 segundo para ser  concluída. 
     setTimeout(()=>{
         const randomNumber = Math.random();
+        //Gera um valor aleatório entre 0 e 1
+
         if(randomNumber > 0.5){
-            resolve(randomNumber); //Resolve a Promise com um valor aleatório   
+            //Se o número gerado for maior que 0.5, considera a operação bem-sucedida 
+
+            resolve(randomNumber); 
+            //Chama 'resolve' para resolver a Promise, passando o número gerado com o resultado. 
+
         } else{
+            //Caso contrario, considera que ocorreu um erro
+
             reject(new Error('Erro')); 
-        }
+        }   //Chama 'reject' para rejeitar a Promise, passando um instância de 'erro' com a mensagem "Erro". 
 
-    },1000)
+    },1000)//Operação simulada ocorre após 1 segundo (1000 ms)
 })
 
 myPromise.then((result)=>{
+    //Adiciona um manipulador que será chamado quando a Promise for resolvida. 
+
     console.log(`Promise resolvida: ${result}`);
-}).catch((error)=>{
+    //Imprime no console o valor retornado pela Promise
+
+})
+.catch((error)=>{
+    //Adiciona um manipulador que será chamado quando a Promise for rejeitada. 
+
     console.error(`Promise rejeitada : ${error}`)
+    //Imprime no console o erro retornado pela Promise
 })
 
 myPromise.then((result)=>{
+    //Este é outro encadeamento 'then', usado para manipular o mesmo resultado da Promise.
+
     console.log(`Primeiro then : ${result}`);
-    return result * 2 //Retorna um valor para ser passado pora o próximo then
+    //Imprime no console o resultado da Promise.
+
+    return result * 2 //Retorna o resuldado multiplicado por 2, que será passado para o próximo 'then'.  
+
+
 }).then((result)=>{
+    //Manipula o valor retornado pelo 'then' anterior. 
+
     console.log(`Segundo then : ${result}`);
-}).catch((error=>{
+    //Imprime no console o valor multiplicado por 2. 
+})
+.catch((error=>{
+    //Captura qualquer erro ocorrido em qualquer etapa da cadeia de 'then'. 
+
     console.error(`Error: ${error}`);
+    //Imprime no console o erro capturado. 
 }))
