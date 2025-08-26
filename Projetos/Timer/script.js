096abfbe9bd65a5511158472347365194c8c4cb2
fagner <fagnerfontesbatista@hotmail.com>
2025-08-25 22:23:57 -0300

    time3 25-08-2025

--- Projetos/Timer/script.js
+++ Projetos/Timer/script.js
@@ -6,7 +6,6 @@ const btn_desativar = document.getElementById("btn_parar");// Pega o botão do H
 const tmp = document.getElementById("tmp_alarme");// Pega a div do HTML onde o timer será exibido
 const btn_alarme = document.getElementById("hora_alarme");// Pega o botão do HTML para ativar o alarme
 
-let timer;// Variável para armazenar o timer
 
 btn_ativar.addEventListener("click", function() {// Adiciona um evento de clique ao botão ativar
     timer = setInterval(function() {// Inicia um timer que chama a função a cada 1000 milissegundos (1 segundo)
@@ -20,14 +19,9 @@ const data = new Date();// Cria um objeto Date com a data e hora atuais
 
 let dia=data.getDate();// Pega o dia do mês
 dia = dia < 10 ? '0' + dia : dia;// Adiciona zero à esquerda se o dia for menor que 10  // Exemplo: 9 -> 09
-
 let mes=data.getMonth() + 1;// Pega o mês (0-11) e adiciona 1 para ficar de 1-12
-
 mes = mes < 10 ? '0' + mes : mes;// Adiciona zero à esquerda se o mês for menor que 10  // Exemplo: 9 -> 09
-
-
 const data_r=dia + '/' + mes + '/' + data.getFullYear();// Formata a data no formato DD/MM/AAAA
-
 div_data.innerHTML = data_r;// Exibe a data na div do HTML
 
 const relogio = () => {
